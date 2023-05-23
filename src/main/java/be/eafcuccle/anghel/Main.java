219c82bff475a3d51f7baef92607983538676ca1
Alexandru Anghel <alexandru@ditar.io>
2023-05-23 10:51:38 +0200

    first commit

--- src/main/java/be/eafcuccle/anghel/Main.java
+++ src/main/java/be/eafcuccle/anghel/Main.java
@@ -1,7 +1,21 @@
 package be.eafcuccle.anghel;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        long startTime = System.nanoTime();
+
+        for (int i = 0; i < 1_000_000; i++) {
+            logger.info("Message DEBUG n°{}", i);
+        }
+
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime) / 1_000_000;  // convert to milliseconds
+        logger.info("Duration: {} ms", duration);
     }
 }
